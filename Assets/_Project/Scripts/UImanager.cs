820c5bc9362fdfd15625ba27b8ad8841877d9494
Pipaliyavivek <75718328+pipaliyavivek@users.noreply.github.com>
2021-12-08 00:13:35 +0530

    Packages added

---
+++ Assets/_Project/Scripts/UImanager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class UImanager : MonoBehaviour
+{
+    public TextMeshProUGUI mCoinText;
+    public TextMeshProUGUI mDaimondText;
+
+    private int coinValue;
+    private int daimondValue;
+
+    public int CoinValue
+    {
+        get { return PlayerPrefs.GetInt(nameof(CoinValue)); }
+        set { PlayerPrefs.SetInt(nameof(CoinValue), value); }
+    }
+    public int DaimondValue
+    {
+        get { return PlayerPrefs.GetInt(nameof(DaimondValue)); }
+        set { PlayerPrefs.SetInt(nameof(DaimondValue), value); }
+    }
+    private void FixedUpdate()
+    {
+        mCoinText.text = CoinValue.ToString();
+        mDaimondText.text = DaimondValue.ToString();
+    }
+    [Button]
+    void setCoinValue(int val)
+    {
+        CoinValue = val;
+    }
+}
